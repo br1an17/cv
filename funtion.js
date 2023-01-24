e83432020c2d00b936c5b566153782237c4ca7c4
br1an17 <106201381+br1an17@users.noreply.github.com>
2023-01-24 18:42:54 -0300

    botones funcionales

--- funtion.js
+++ funtion.js
@@ -1,26 +1,23 @@
-let ocultar_tecnologias = document.getElementById("ocultarTecnologias")
-
-let tegnologias = document.getElementById("tecnologias")
-
-
-ocultar_tecnologias.addEventListener("click",toggleTec)
+let imagen = document.getElementById("miImagen");
+let angulo = document.getElementById("angulo");
+let apellido = document.getElementById("apellido");
 
 
+function cambiarATech() {
+  document.getElementById("tech").style.display = "block";
+  document.getElementById("clasico").style.display = "none";
+  document.getElementById("oficio").innerText = "Desarrollador Full-Stack";
+  
+  angulo.style.background = "red";
+  imagen.src = "../cv/img/mias/mia1.jpg";
+  apellido.style.color= "red"
 
-function toggleTec() {
-tegnologias.classList.toggle("mostrar")
 }
 
-
-let ocultar_redes = document.getElementById("ocultarRedes")
-
-let contacto = document.getElementById("contacto")
-
-
-ocultar_redes.addEventListener("click",toggleRedes)
-
-
-
-function toggleRedes() {
-    contacto.classList.toggle("mostrar")
+function cambiarAClass() {
+  document.getElementById("clasico").style.display = "block";
+  document.getElementById("tech").style.display = "none";
+  imagen.src = "../cv/img/mias/brian.jpg";
+  angulo.style.background = "#2fb7fe"
+  apellido.style.color=  "#2fb7fe"
 }
