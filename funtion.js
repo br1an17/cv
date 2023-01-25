388af622a4fdda0b6b26472f1a9bb3627340110d
br1an17 <106201381+br1an17@users.noreply.github.com>
2023-01-25 19:05:28 -0300

    falta menos

--- funtion.js
+++ funtion.js
@@ -1,5 +1,6 @@
 let imagen = document.getElementById("miImagen");
 let angulo = document.getElementById("angulo");
+let anguloInferior = document.getElementById("anguloI");
 let apellido = document.getElementById("apellido");
 
 
@@ -7,17 +8,19 @@ function cambiarATech() {
   document.getElementById("tech").style.display = "block";
   document.getElementById("clasico").style.display = "none";
   document.getElementById("oficio").innerText = "Desarrollador Full-Stack";
-  
-  angulo.style.background = "red";
+  anguloInferior.style.background = "#6f000c"
+  angulo.style.background = "#a42500";
   imagen.src = "../cv/img/mias/mia1.jpg";
-  apellido.style.color= "red"
+  apellido.style.color= "#a42500"
 
 }
 
 function cambiarAClass() {
   document.getElementById("clasico").style.display = "block";
   document.getElementById("tech").style.display = "none";
+  document.getElementById("oficio").innerText = "DEDICADO Y PERSEVERANTE";
   imagen.src = "../cv/img/mias/brian.jpg";
+  anguloInferior.style.background = "#3d3f42"
   angulo.style.background = "#2fb7fe"
   apellido.style.color=  "#2fb7fe"
 }
