d6a59b5b3f12a6fe7c97c3e54b20b90d79b49023
br1an17 <106201381+br1an17@users.noreply.github.com>
2023-01-26 18:13:25 -0300

    ultimos cambios

--- funtion.js
+++ funtion.js
@@ -2,7 +2,9 @@ let imagen = document.getElementById("miImagen");
 let angulo = document.getElementById("angulo");
 let anguloInferior = document.getElementById("anguloI");
 let apellido = document.getElementById("apellido");
-
+let dark =document.getElementById("dark")
+let main_body = document.getElementById("right-section");
+let linea =document.getElementById("total")
 
 function cambiarATech() {
   document.getElementById("tech").style.display = "block";
@@ -12,15 +14,24 @@ function cambiarATech() {
   angulo.style.background = "#a42500";
   imagen.src = "../cv/img/mias/mia1.jpg";
   apellido.style.color= "#a42500"
+  main_body =document.getElementById("right-section2")
 
 }
 
 function cambiarAClass() {
-  document.getElementById("clasico").style.display = "block";
   document.getElementById("tech").style.display = "none";
+  document.getElementById("clasico").style.display = "block";
   document.getElementById("oficio").innerText = "DEDICADO Y PERSEVERANTE";
   imagen.src = "../cv/img/mias/brian.jpg";
   anguloInferior.style.background = "#3d3f42"
   angulo.style.background = "#2fb7fe"
   apellido.style.color=  "#2fb7fe"
+  main_body =document.getElementById("right-section")
+}
+
+function darkMode(){
+  if(
+  main_body.classList.toggle("dark-mode"))
+  linea.style.background = ("#252525")
+  else( linea.style.background = ("#f4f7ff"))
 }
